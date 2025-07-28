87d65e650277e9e72247ff7afc931c96b6fb9ba1
syndromme <agus.rusty3@gmail.com>
2025-07-28 15:01:18 +0700

    project generated

---
+++ pokemon-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemon-ios
+//
+//  Created by syndromme on 28/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pokemon-iosTests/pokemon_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_iosTests.swift
+//  pokemon-iosTests
+//
+//  Created by syndromme on 28/07/25.
+//
+
+import Testing
+@testable import pokemon_ios
+
+struct pokemon_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
