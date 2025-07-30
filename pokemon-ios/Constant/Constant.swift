584ee6608ee8e7226e15a7d43754edcbdc13a816
syndromme <agus.rusty3@gmail.com>
2025-07-30 12:21:24 +0700

    feat (CONSTANT): set artwork url

---
+++ pokemon-ios/Constant/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  pokemon-ios
+//
+//  Created by syndromme on 30/07/25.
+//
+
+let artworkURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/%d.png"
