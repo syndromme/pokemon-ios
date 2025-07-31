229a2b92221df6a6e03ac04e631b6e6f963c897c
syndromme <agus.rusty3@gmail.com>
2025-07-31 15:53:54 +0700

    feat (EXTENSION): create string extension

---
+++ pokemon-ios/Extension/String+Extension.swift
@@ -0,0 +1,58 @@
+//
+//  String+Extension.swift
+//  pokemon-ios
+//
+//  Created by syndromme on 30/07/25.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    
+    //To check text field or String is blank or not
+    var isBlank: Bool {
+        get {
+            let trimmed = trimmingCharacters(in: CharacterSet.whitespaces)
+            return trimmed.isEmpty
+        }
+    }
+    
+    //Validate Email
+    var isEmail: Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
+            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil
+        } catch {
+            return false
+        }
+    }
+    
+    //Validate Phone
+    var isPhone: Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "^(\\+62|62|0)8[1-9]{1}\\d{1}[\\s-]?\\d{4}[\\s-]?\\d{2,5}$", options: .caseInsensitive)
+            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil
+        } catch {
+            return false
+        }
+    }
+    
+    var isAlphanumeric: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
+    
+    //validate Password
+    var isValidPassword: Bool {
+        let pattern = #"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+=\[{\]};:<>|./?,-]).{6,}$"#
+        let predicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+        return predicate.evaluate(with: self)
+        
+//        do {
+//            let regex = try NSRegularExpression(pattern: #"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':\\|,.<>/?]).{8,}$"#, options: .caseInsensitive)
+//            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil
+//        } catch {
+//            return false
+//        }
+    }
+}
