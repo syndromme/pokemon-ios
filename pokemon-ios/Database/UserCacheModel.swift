13e8a881d0d99251ff6e6f3e5f70f681bd76186b
syndromme <agus.rusty3@gmail.com>
2025-07-31 15:53:24 +0700

    feat (DB): create user cache model

---
+++ pokemon-ios/Database/UserCacheModel.swift
@@ -0,0 +1,22 @@
+//
+//  UserCacheModel.swift
+//  pokemon-ios
+//
+//  Created by syndromme on 31/07/25.
+//
+
+import RealmSwift
+
+class UserCacheModel: Object {
+
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var firstName: String = ""
+    @Persisted var lastName: String = ""
+    @Persisted var email: String = ""
+    @Persisted var phoneNumber: String = ""
+    @Persisted var password: String = ""
+
+    func toUser() -> Login.UseCase.Response {
+        return .init(firstName: firstName, lastName: lastName, email: email, phoneNumber: phoneNumber)
+    }
+}
