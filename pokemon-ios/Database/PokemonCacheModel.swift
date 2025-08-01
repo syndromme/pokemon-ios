bfda1154309d12d7ccbea459b855b41ab01865e4
syndromme <agus.rusty3@gmail.com>
2025-08-02 01:33:14 +0700

    fix (REPOSITORY): handle offline pokemon data

--- pokemon-ios/Database/PokemonCacheModel.swift
+++ pokemon-ios/Database/PokemonCacheModel.swift
@@ -11,29 +11,47 @@ class PokemonCacheModel: Object {
     @Persisted var id: Int = 0
     @Persisted var name: String = ""
     @Persisted var url: String = ""
-    @Persisted var baseExperience: Int = 0
-    @Persisted var height: Int = 0
-    @Persisted var weight: Int = 0
+    @Persisted var baseExperience: Int?
+    @Persisted var height: Int?
+    @Persisted var weight: Int?
     @Persisted var statistics: List<StatisticsCacheModel>
+    @Persisted var types: List<TypeCacheModel>
+    @Persisted var abilities: List<AbilityCacheModel>
 
     override static func primaryKey() -> String? {
         return "name"
     }
 
     func toPokemon() -> Pokemon {
-        return .init(id: id, name: name, url: url, baseExperience: baseExperience, height: height, weight: weight, stats: [], types: [], abilities: [])
+        return .init(id: id,
+                     name: name,
+                     url: url,
+                     baseExperience: baseExperience,
+                     height: height,
+                     weight: weight,
+                     stats: statistics.map({ $0.toStatistics() }),
+                     types: types.map({ $0.toMonsterType() }),
+                     abilities: abilities.map({ $0.toMonsterAbility()})
+        )
     }
 
     static func from(pokemon: Pokemon) -> PokemonCacheModel {
         let model = PokemonCacheModel()
+        model.id = Int(pokemon.url?.split(separator: "/").last ?? "") ?? 0
         model.name = pokemon.name
         model.url = pokemon.url ?? ""
-//        model.baseExperience = pokemon?.baseExperience
-//        model.height = pokemon?.height
-//        model.weight = pokemon?.weight
-//        model.statistics = pokemon.statistics.map({ statistic in
-//            StatisticsCacheModel(value: statistic)
-//        })//(StatisticsCacheModel.init)
+        model.baseExperience = pokemon.baseExperience
+        model.height = pokemon.height
+        model.weight = pokemon.weight
+        pokemon.stats?.forEach({ stat in
+            model.statistics.append(StatisticsCacheModel.from(statistic: stat))
+        })
+        pokemon.types?.forEach({ type in
+            model.types.append(TypeCacheModel.from(value: type))
+        })
+        pokemon.abilities?.forEach({ ability in
+            model.abilities.append(AbilityCacheModel.from(value: ability))
+        })
         return model
     }
 }
@@ -41,18 +59,67 @@ class PokemonCacheModel: Object {
 class StatisticsCacheModel: Object {
     @Persisted var baseStat: Int = 0
     @Persisted var effort: Int = 0
-    @Persisted var stat: StatisticCacheModel?
+    @Persisted var stat: DefaultCacheModel?
     
     func toStatistics() -> Statistics {
-        .init(baseStat: baseStat, effort: effort, stat: stat!.toStatistic())
+        .init(baseStat: baseStat, effort: effort, stat: stat!.toDefault())
+    }
+    
+    static func from(statistic: Statistics) -> StatisticsCacheModel {
+        let model = StatisticsCacheModel()
+        model.baseStat = statistic.baseStat
+        model.effort = statistic.effort
+        model.stat = DefaultCacheModel.from(value: statistic.stat)
+        return model
     }
 }
 
-class StatisticCacheModel: Object {
+class TypeCacheModel: Object {
+    @Persisted var slot: Int = 0
+    @Persisted var type: DefaultCacheModel?
+    
+    func toMonsterType() -> MonsterType {
+        .init(slot: slot, type: type!.toDefault())
+    }
+    
+    static func from(value: MonsterType) -> TypeCacheModel {
+        let model = TypeCacheModel()
+        model.slot = value.slot
+        model.type = DefaultCacheModel.from(value: value.type)
+        return model
+    }
+}
+
+class AbilityCacheModel: Object {
+    @Persisted var slot: Int = 0
+    @Persisted var isHidden: Bool = false
+    @Persisted var ability: DefaultCacheModel?
+    
+    func toMonsterAbility() -> MonsterAbility {
+        .init(slot: slot, isHidden: isHidden, ability: ability!.toDefault())
+    }
+    
+    static func from(value: MonsterAbility) -> AbilityCacheModel {
+        let model = AbilityCacheModel()
+        model.slot = value.slot
+        model.isHidden = value.isHidden
+        model.ability = DefaultCacheModel.from(value: value.ability)
+        return model
+    }
+}
+
+class DefaultCacheModel: Object {
     @Persisted var name: String = ""
     @Persisted var url: String = ""
     
-    func toStatistic() -> DefaultResponse {
+    func toDefault() -> DefaultResponse {
         .init(name: name, url: url)
+    }
+    
+    static func from(value: DefaultResponse) -> DefaultCacheModel {
+        let model = DefaultCacheModel()
+        model.name = value.name
+        model.url = value.url
+        return model
     }
 }
--- pokemon-ios/Repository/PokemonRepository.swift
+++ pokemon-ios/Repository/PokemonRepository.swift
@@ -10,16 +10,20 @@ import RealmSwift
 
 class PokemonRepository {
     private let apiProvider: APIProviderProtocol
+    private let reachability = ReachabilityService.shared
+    private let realm = try! Realm()
 
     init(apiProvider: APIProviderProtocol = APIProvider()) {
         self.apiProvider = apiProvider
     }
 
     func getPokemons(request: Dashboard.UseCase.Request) -> Observable<Dashboard.UseCase.Response> {
+        if !reachability.currentStatus() {
+            return self.loadListFromCache()
+        }
         return apiProvider.execute(APIService.fetch(name: request.name, limit: request.limit, offset: request.offset), decodeTo: Dashboard.UseCase.Response.self)
             .do(onNext: { response in
                 self.saveToCache(response.results)
-                
             }).catch { error in
                 print("⚠️ API failed, falling back to cache: \(error.localizedDescription)")
                 return self.loadListFromCache()
@@ -27,20 +31,20 @@ class PokemonRepository {
     }
     
     func getPokemon(request: Dashboard.UseCase.Request) -> Observable<Pokemon?> {
-        var pokemon: Pokemon?
+        if !reachability.currentStatus() {
+            return self.loadItemFromCache(name: request.name ?? "")
+        }
         return apiProvider.execute(APIService.fetch(name: request.name, offset: nil), decodeTo: Pokemon?.self)
             .do(onNext: { response in
-                pokemon = response
-                self.updateCache(pokemon!)
+                self.updateCache(response!)
             }).catch { error in
                 print("⚠️ API failed, falling back to cache: \(error.localizedDescription)")
-                return Observable.just(pokemon)
+                return self.loadItemFromCache(name: request.name ?? "")
             }
     }
 
     private func saveToCache(_ pokemons: [Pokemon]) {
         do {
-            let realm = try Realm()
             try realm.write {
                 realm.add(pokemons.map(PokemonCacheModel.from), update: .modified)
             }
@@ -51,30 +55,48 @@ class PokemonRepository {
 
     private func loadListFromCache() -> Observable<Dashboard.UseCase.Response> {
         do {
-            let realm = try Realm()
             let cached = realm.objects(PokemonCacheModel.self)
             let pokemons = cached.map { $0.toPokemon() }
             return Observable.just(Dashboard.UseCase.Response.init(count: pokemons.count, results: Array(pokemons)))
         } catch {
+            print("❌ Failed to load cache: \(error)")
+            return Observable.error(error)
+        }
+    }
+    
+    private func loadItemFromCache(name: String) -> Observable<Pokemon?> {
+        do {
+            if let pokemon = realm.object(ofType: PokemonCacheModel.self, forPrimaryKey: name.lowercased()) {
+                return .just(pokemon.toPokemon())
+            }
+            return .just(nil)
+        } catch {
+            print("❌ Failed to load cache: \(error)")
             return Observable.error(error)
         }
     }
     
     private func updateCache(_ pokemon: Pokemon) {
         do {
-            let realm = try Realm()
             let cached = realm.objects(PokemonCacheModel.self)
-            guard let cachedPokemon = cached.where{ $0.name == pokemon.name}.first else { return }
+            guard let cachedPokemon = cached.where({ $0.name == pokemon.name}).first else { return }
             
             try realm.write {
-                cachedPokemon.id = pokemon.id ?? 0
                 cachedPokemon.baseExperience = pokemon.baseExperience ?? 0
                 cachedPokemon.height = pokemon.height ?? 0
                 cachedPokemon.weight = pokemon.weight ?? 0
-//                cachedPokemon.statistics
+                pokemon.stats?.forEach({ stat in
+                    cachedPokemon.statistics.append(StatisticsCacheModel.from(statistic: stat))
+                })
+                pokemon.types?.forEach({ type in
+                    cachedPokemon.types.append(TypeCacheModel.from(value: type))
+                })
+                pokemon.abilities?.forEach({ ability in
+                    cachedPokemon.abilities.append(AbilityCacheModel.from(value: ability))
+                })
             }
         } catch {
-            print("❌ Failed to save cache: \(error)")
+            print("❌ Failed to update cache: \(error)")
         }
     }
 }
